7ed6f9e8db85a7cf3b4bd03ac08846a48e6a4816
Luiz Lima Dev <109322803+LuizLimaDev@users.noreply.github.com>
2022-12-27 08:47:02 -0300

    Upgrad funcionalidades
    1. Adicionado o botao "Copiar".
    2. Adicionada a validação de texto.

--- src/Js/main.js
+++ src/Js/main.js
@@ -3,11 +3,9 @@
 // - Deve funcionar apenas com letras minúsculas
 // - Não devem ser utilizados letras com acentos nem caracteres especiais
 
-// 2. Exibir mensagem de erro para a entradas com caracteres invalidos ou vazio
-// - Throw error 
-// - try e tach
+// 2. Botao copiar
 
-// 3. Botao copiar
+// 3. Exibir cor vermelha no texto de alerta, caso nao passe pelo REGEX
 
 //seletores globais
 let campoTexto = document.querySelector('.decodificador__campoTexto');
@@ -15,7 +13,31 @@ const msgInicial = document.querySelector('[data-msgInicial');
 const msgCodificada = document.querySelector('[data-msgCodificada]');
 const imgRadar = document.querySelector('[data-criptografia-img]');
 const msgCriptografiaTextos = document.querySelector('[data-criptografia-textos]');
+const alerta = document.querySelector('[data-alerta]');
 const campoCodificacao = document.querySelector('.container__campoTextoCodificado');
+
+//validacao
+const regexPattern = /[^a-z\s]+/g;
+const btnCriptografar = document.querySelector('[data-cripitografar]');
+
+btnCriptografar.addEventListener('click', function(event) {
+    event.preventDefault();
+    validacao();
+});
+
+campoTexto.addEventListener('input', validacao);
+
+function validacao() {
+    if (campoTexto.value.match(regexPattern)) {
+        alerta.classList.add('texto__invalido');
+        document.querySelector('[data-cripitografar]').disabled = true;
+        document.querySelector('[data-descripitografar]').disabled = true;
+    } else {
+        alerta.classList.remove('texto__invalido');
+        document.querySelector('[data-cripitografar]').disabled = false;
+        document.querySelector('[data-descripitografar]').disabled = false;
+    }
+}
 
 //Criptografia
 function criptografar() {
@@ -33,21 +55,22 @@ function criptografar() {
             .replace(/u/gim, "ufat");
 
         //retorno na tela
-        msgInicial.style= "display:none";
-        campoCodificacao.style= "display:flex";
-        imgRadar.style= "display:none";
-        msgCriptografiaTextos.style= "display:none";
+        msgInicial.style = "display:none";
+        campoCodificacao.style = "display:flex";
+        imgRadar.style = "display:none";
+        msgCriptografiaTextos.style = "display:none";
 
         msgCodificada.innerHTML = `
             <textarea class="campoTextoCodificado" cols="70" rows="30">${criptografia}</textarea>
-            <div class="container__copiar">
-                <button class="botoes copiar" data-copiar><img src="./src/img/copy.svg" alt="simbolo de copiar"
-                class="botoes__copiar-img"> Copiar </button>
-            </div>`
+            <div class="container__copiar"></div>`
+
+        copiar();
+
     });
 }
 
-function descriptografar () {
+//Descriptografia
+function descriptografar() {
     //seletores
     const btnDescriptografar = document.querySelector('[data-descripitografar]');
     btnDescriptografar.addEventListener('click', () => {
@@ -62,20 +85,36 @@ function descriptografar () {
             .replace(/ufat/gim, "u");
 
         //retorno na tela
-        msgInicial.style= "display:none";
-        campoCodificacao.style= "display:flex";
-        imgRadar.style= "display:none";
-        msgCriptografiaTextos.style= "display:none";
+        msgInicial.style = "display:none";
+        campoCodificacao.style = "display:flex";
+        imgRadar.style = "display:none";
+        msgCriptografiaTextos.style = "display:none";
 
         msgCodificada.innerHTML = `
             <textarea class="campoTextoCodificado" cols="70" rows="30">${resultado}</textarea>
-            <div class="container__copiar">
-                <button class="botoes copiar" data-copiar><img src="./src/img/copy.svg" alt="simbolo de copiar"
-                class="botoes__copiar-img"> Copiar </button>
-            </div>`
+            <div class="container__copiar"></div>`
+
+        copiar();
+
     });
 }
 
+//Botao copiar
+function copiar() {
+    const copiar = document.querySelector('[data-copiar]');
+    copiar.style = "display: flex";
+    copiar.addEventListener('click', () => {
 
+        let textoCodificado = document.querySelector('.campoTextoCodificado');
+        textoCodificado.select();
+        document.execCommand('copy');
+
+        textoCodificado.value = "";
+
+    });
+}
+
+//Chamada das funções
 criptografar();
 descriptografar();
+
